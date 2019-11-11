90005d6225374222cb6c81a09db8fe6504a2f84e
Student <student@donation.local>
2019-11-11 13:59:46 -0600

    Django benchmark in class.

--- app/views.py
+++ app/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from app.models import Movie, Ticket, Showing
+from app.forms import NewTicketForm
+
 
 # Create your views here.
+
+
+def home(request):
+    movies = Movie.objects.all()
+    return render(request, "home.html", {"movies": movies})
+
+
+def new_ticket(request, id):
+    if request.method == "GET":
+        movie = Movie.objects.get(id=id)
+        form = NewTicketForm()
+        return render(request, "new_ticket.html", {"movie": movie, "form": form})
+    elif request.method == "POST":
+        form = NewTicketForm(request.POST)
+        if form.is_valid():
+            movie = Movie.objects.get(id=id)
+            name = form.cleaned_data['name']
+            showing_id = form.cleaned_data['showing_id']
+            new_ticket = Ticket.objects.create(
+                name=name, showing_id=movie.id, purchased_at=timezone.now()
+            )
+            new_ticket.save()
+            return redirect("ticket_detail", movie.id)
+        else:
+            movie = Movie.objects.get(id=id)
+            form = NewTicketForm()
+            return render(request, "new_ticket.html", {"movie": movie, "form": form})
+
+
+def ticket_detail(request, id):
+    ticket = Ticket.objects.get(id=id)
+    return render(request, "ticket_detail.html", {"ticket": ticket})
